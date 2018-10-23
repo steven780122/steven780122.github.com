b675a896a0f6b6ff82bf44a02b2b84204f362d20
Steven Hsiao <stevenhsiao@starlux-airlines.com>
2018-10-23 14:32:50 +0800

    update to normal nodejs server

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let app = express();
+
+
+app.use(express.static("www"))    // 設定資料夾www為靜態檔案的資料夾
+
+// 啟動伺服器在 localhost:3000
+// 如果要上線不是要測試可以打80，使用者就不用再打port
+app.listen(3000, function(){   
+    // 伺服器啟動成功的回呼函式
+    console.log("server ok!");
+});   
+
---
+++ module.js
@@ -0,0 +1,5 @@
+// module.exports=3;   //固定的，跟檔名無關!!
+
+module.exports=function(name){
+    return "Hello " + name
+};
